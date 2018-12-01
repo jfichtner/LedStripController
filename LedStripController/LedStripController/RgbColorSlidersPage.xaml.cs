1c344483ff856b1101c5c3f0be2645085502780a
Justin Fichtner <Justin.Fichtner@lakeshore.com>
2018-12-01 16:30:04 -0500

    Make sliders work on the fly

--- LedStripController/LedStripController/RgbColorSlidersPage.xaml.cs
+++ LedStripController/LedStripController/RgbColorSlidersPage.xaml.cs
@@ -1,61 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using Xamarin.Forms;
 
 namespace LedStripController
 {
     public partial class RgbColorSlidersPage : ContentPage
     {
-        TcpClient tcpClient;
-        NetworkStream clientStream;
-        StreamWriter streamWriter;
-        int red;
-        int blue;
-        int green;
+        private readonly TcpClient tcpClient = new TcpClient();
+        private readonly StreamWriter streamWriter;
+        private int red;
+        private int blue;
+        private int green;
+        private readonly Timer delayTimer = new Timer(200);
+        private bool delay;
 
         public RgbColorSlidersPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.tcpClient.Connect("192.168.0.143", 9999);
+            this.streamWriter = new StreamWriter(this.tcpClient.GetStream());
+            this.streamWriter.AutoFlush = true;
+            this.delayTimer.Elapsed += this.DelayTimer_Elapsed;
         }
 
-        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        private void DelayTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            if (sender == redSlider)
-            {
-                red = (int)args.NewValue;
-                redLabel.Text = String.Format("Red = {0:X2}", red);
-                
-            }
-            else if (sender == greenSlider)
-            {
-                green = (int)args.NewValue;
-                greenLabel.Text = String.Format("Green = {0:X2}", green);
-            }
-            else if (sender == blueSlider)
-            {
-                blue = (int)args.NewValue;
-                blueLabel.Text = String.Format("Blue = {0:X2}", blue);
-            }
-
-            boxView.Color = Color.FromRgb((int)redSlider.Value,
-                                          (int)greenSlider.Value,
-                                          (int)blueSlider.Value);
+            this.delay = false;
+            this.SendRgb();
         }
 
-        void OnButtonClicked(object sender, EventArgs args)
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
         {
-            tcpClient = new TcpClient();
-            tcpClient.Connect("192.168.0.143", 9999);
-            clientStream = tcpClient.GetStream();
-            streamWriter = new StreamWriter(clientStream);
-            streamWriter.Write($"rgb " + red + " " + green + " " + blue);
-            streamWriter.Flush();
-            tcpClient.Close();
+            if (sender == this.redSlider)
+            {
+                this.red = (int)args.NewValue;
+                this.redLabel.Text = String.Format("Red = {0:X2}", this.red);
+                this.SendRgb();
+            }
+            else if (sender == this.greenSlider)
+            {
+                this.green = (int)args.NewValue;
+                this.greenLabel.Text = String.Format("Green = {0:X2}", this.green);
+                this.SendRgb();
+            }
+            else if (sender == this.blueSlider)
+            {
+                this.blue = (int)args.NewValue;
+                this.blueLabel.Text = String.Format("Blue = {0:X2}", this.blue);
+                this.SendRgb();
+            }
+
+            this.boxView.Color = Color.FromRgb((int) this.redSlider.Value,
+                                          (int) this.greenSlider.Value,
+                                          (int) this.blueSlider.Value);
+        }
+
+        private void SendRgb()
+        {
+            if (!this.delay)
+            {
+                this.streamWriter.Write($"rgb " + this.red + " " + this.green + " " + this.blue);
+            }
+
+            this.delay = true;
+            this.delayTimer.Start();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs args)
+        {
+            this.SendRgb();
         }
     }
 }
